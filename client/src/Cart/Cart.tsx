4cf0e1002e5181580f5deb4b7da3f52e99a07cba
Gianni Nola <53386620+Gnola@users.noreply.github.com>
2022-02-03 15:39:39 -0800

    Added Total functionality to cart

--- client/src/Cart/Cart.tsx
+++ client/src/Cart/Cart.tsx
@@ -10,6 +10,9 @@ type Props = {
 }
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
+  const calculateTotal = (items: CartItemType[]) => {
+    return items.reduce((ack: number, item) => ack + item.amount * item.price, 0)
+  }
   return (
     <Wrapper>
       <h2>Your Shopping Cart</h2>
@@ -21,6 +24,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
           <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart}/>
         ))
       }
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   )
 }
